54c74850a69a52ab3356f9ca2a580cd73c303638
josepaniagua <jpanjr@gmail.com>
2019-08-20 21:18:36 -0700

    rendered initial scores

--- src/App.js
+++ src/App.js
@@ -18,14 +18,15 @@ function App() {
           <div className="home">
             <h2 className="home__name">Lions</h2>
 
-            {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
+            {/* TODO STEP 3 - We need to change the hardcoded values in these divs
+            to accept dynamic values from our state. */}
 
-            <div className="home__score">32</div>
+            <div className="home__score">{lionsScore}</div>
           </div>
           <div className="timer">00:03</div>
           <div className="away">
             <h2 className="away__name">Tigers</h2>
-            <div className="away__score">32</div>
+            <div className="away__score">{tigersScore}</div>
           </div>
         </div>
         <BottomRow />
