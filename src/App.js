c372582e09626cb9df9505465138c95752775e41
josepaniagua <jpanjr@gmail.com>
2019-08-20 21:05:57 -0700

    set state per team

--- src/App.js
+++ src/App.js
@@ -4,7 +4,12 @@ import "./App.css";
 import BottomRow from "./BottomRow"
 
 function App() {
-  //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
+  //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.
+  // You'll need one for the home score and another for the away score.
+
+  // Initial state per team
+ const [lionsScore, updateLionsScore] = useState(0);
+ const [tigersScore, updateTigersScore] = useState(0);
 
   return (
     <div className="container">
