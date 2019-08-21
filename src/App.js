cd01cea11e68a8edf35cbeb785c097041abc8a08
josepaniagua <jpanjr@gmail.com>
2019-08-20 22:53:56 -0700

    school colors

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ function App() {
       <section className="scoreboard">
         <div className="topRow">
           <div className="home">
-            <h2 className="home__name">Lions</h2>
+            <h2 className="home__name">USC</h2>
 
             {/* TODO STEP 3 - We need to change the hardcoded values in these divs
             to accept dynamic values from our state. */}
@@ -43,7 +43,7 @@ function App() {
           </div>
           <div className="timer">00:03</div>
           <div className="away">
-            <h2 className="away__name">Tigers</h2>
+            <h2 className="away__name">UCLA</h2>
             <div className="away__score">{tigersScore}</div>
           </div>
         </div>
